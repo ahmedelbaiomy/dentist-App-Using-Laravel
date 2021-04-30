0f5f73cc2c9deff82d965a6082728b51ddc67207
hidaoui mouhssin <hidaoui.mouhssin@gmail.com>
2021-04-30 23:04:57 +0000

    debug

--- app/Http/Controllers/Reception/HomeController.php
+++ app/Http/Controllers/Reception/HomeController.php
@@ -216,16 +216,13 @@ class HomeController extends Controller
             $original_name=$uploadedFile->getClientOriginalName();
             $size=$uploadedFile->getSize();
             
-
-
             $path = 'uploads/files/audio/';
             if(!File::exists($path)) {
                 File::makeDirectory($path, 0755, true, true);
             }
 
-            
-			
-			Storage::disk ( 'public' )->putFileAs ( $path, $uploadedFile, $original_name );
+			$p=Storage::disk ( 'public' )->putFileAs ( $path, $uploadedFile, $original_name );
+            dd($p);
 			$exists = Storage::disk ( 'public' )->exists ( $path."{$original_name}" );
 			 if ($exists) {
 				dd('success');
