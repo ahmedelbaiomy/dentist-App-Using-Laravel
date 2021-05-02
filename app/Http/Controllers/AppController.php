9fd302c40d6df4d8930ae83d36cd1aba416fc71e
hidaoui mouhssin <hidaoui.mouhssin@gmail.com>
2021-05-02 17:20:20 +0000

    storage

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\note;
 use App\Models\User;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Http\Request;
 use App\Library\Helpers\Helper;
 use Illuminate\Support\Facades\DB;
