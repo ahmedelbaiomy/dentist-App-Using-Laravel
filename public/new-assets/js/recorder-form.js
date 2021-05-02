8e3a8eaa5b737d1c84853f2215e086551f5812e1
hidaoui mouhssin <hidaoui.mouhssin@gmail.com>
2021-05-02 00:30:22 +0000

    BEFORE

--- public/new-assets/js/recorder-form.js
+++ public/new-assets/js/recorder-form.js
@@ -122,6 +122,7 @@ function stopRecording() {
 function createDownloadLink(blob) {
 	
 	var url = URL.createObjectURL(blob);
+	console.log('BEFORE='+blob);
 	$('#BLOB_FILE').val(blob);
 	var au = document.createElement('audio');
 	var li = document.createElement('li');
